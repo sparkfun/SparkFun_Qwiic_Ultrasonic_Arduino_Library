f217af91d836f74662a1e8a5b03b349d2a224acb
Dryw Wade <dryw.wade@sparkfun.com>
2023-12-19 10:50:35 -0700

    Initial commit

---
+++ src/SparkFun_Template_Arduino_Library.cpp
@@ -0,0 +1,16 @@
+#include "SparkFun_Template_Arduino_Library.h"
+
+BMI270::BMI270()
+{
+    // TODO: Initialize variables
+}
+
+int8_t BMI270::begin()
+{
+    // TODO: Check if device is connected
+
+    // TODO: Initialize hardware
+
+    // TODO: Return whether successful
+    return false;
+}
